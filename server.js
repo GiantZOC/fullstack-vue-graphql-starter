e327ebe662cd80109700f808292838f85253eb6c
steven <steven@giantzoc.com>
2019-07-26 13:55:46 -0500

    Lesson 9

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
-const {ApolloServer} = require('apollo-server');
+const {ApolloServer, gql} = require('apollo-server');
 
 const todos = [
  {task: 'Wash car', completed:false},
- {task: 'Clean room', completed:true}
+ {task: 'Clean room', completed:true},
 ]
 
 const typeDefs = gql`
-type Todo {
-    task: String
-    completed: Boolean
-}
+    type Todo {
+        task: String
+        completed: Boolean
+    }
 
-type Query {
-    getTodos: [Todo]
-}
+    type Query {
+        getTodos: [Todo]
+    }
+
+    type Mutation {
+        addTodo(task: String, completed: Boolean)
+    }
 `;
 
 const resolvers = {
     Query: {
         getTodoes: () => todos
+    },
+    Mutation: {
+        addTodo: (_, {task, completed}) => {
+            const todo = {task, completed};
+            todos.push(todo);
+            return todo;
+        }
     }
 }
 
