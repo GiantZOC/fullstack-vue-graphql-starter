8983f5a88c4954aec949db287a97fd0958116ac8
steven <steven@giantzoc.com>
2019-07-26 15:08:37 -0500

    Lesson 11

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const {ApolloServer, gql} = require('apollo-server');
+const mongoose = require('mongoose');
 
-const todos = [
- {task: 'Wash car', completed:false},
- {task: 'Clean room', completed:true},
-]
+require('dotenv').config();
+console.log(process.env.MONGO_URI)
+mongoose
+.connect(process.env.MONGO_URI, {useNewUrlParser: true})
+.then(() => console.log('DB connected'))
+.catch(err => console.error(err));
 
 const typeDefs = gql`
     type Todo {
@@ -15,27 +18,11 @@ const typeDefs = gql`
         getTodos: [Todo]
     }
 
-    type Mutation {
-        addTodo(task: String, completed: Boolean)
-    }
 `;
 
-const resolvers = {
-    Query: {
-        getTodoes: () => todos
-    },
-    Mutation: {
-        addTodo: (_, {task, completed}) => {
-            const todo = {task, completed};
-            todos.push(todo);
-            return todo;
-        }
-    }
-}
 
 const server = new ApolloServer({
-    typeDefs: typeDefs,
-    resolvers: resolvers
+    typeDefs: typeDefs
 });
 
 //initialize server
