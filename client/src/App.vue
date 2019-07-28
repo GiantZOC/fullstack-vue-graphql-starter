75e304406bd1c0232143fd328c2c4deab4c0b02c
steven <steven@giantzoc.com>
2019-07-28 13:33:28 -0500

    Lesson 22 Vuetify with toolbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,70 @@
 <template>
-  <div id="app">
-    <h1>App</h1>
-    <router-view/>
-  </div>
+  <v-app>
+     <v-app-bar app>
+
+
+      <v-toolbar fixed color="primary" dark>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="hidden-xs-only">
+        <router-link to="/" tag="span" style="cursor: pointer">
+        VueShare
+        </router-link>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-text-field flex prepend-icon="mdi-magnify" placeholder="Search posts" color="accent" single-line-hide-details></v-text-field>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items class="hidden-xs-only" color="primary">
+        <v-btn depressed class="v-btn--flat" v-for="item in horizontalNavItems" :key="item.title" :to="item.link">
+          <v-icon class="hidden-sm-only" left>{{item.icon}}</v-icon>
+          {{item.title}}
+        </v-btn>
+        <!-- <v-btn flat>
+          Posts
+        </v-btn>
+           -->
+      </v-toolbar-items>
+
+      </v-toolbar>
+      <!-- <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn> -->
+     </v-app-bar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
+<script>
+
+export default {
+  name: 'App',
+  computed:{
+    horizontalNavItems(){
+      return[
+        { icon: 'mdi-chat', title: 'Posts', link: '/posts'},
+        {icon: 'mdi-login', title: 'Sign In', link: '/signin'},
+        {icon: 'mdi-account', title: 'Sign Up', link: '/signup'}
+      ]
+    }
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ client/src/plugins/vuetify.js
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+
+import colors from 'vuetify/es5/util/colors'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.purple,
+    secondary: colors.grey.darken1,
+    accent: colors.shades.black,
+    error: colors.red.accent3
+  }
+})
+
+// Vue.use(Vuetify,
+//   {theme: {
+//     primary: '#1976D2',
+//     secondary: '#424242',
+//     accent: '#82B1FF',
+//     error: '#FF5252',
+//     info: '#2196F3',
+//     success: '#4CAF50',
+//     warning: '#FFC107'
+//   }});
+
+export default new Vuetify({
+  icons: {
+    iconfont: 'mdi',
+  },
+});
