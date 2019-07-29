56172b41ae5b440f0f4e0debad04fc0d1cf70983
steven <steven@giantzoc.com>
2019-07-29 11:42:24 -0500

    Lesson 26 smart queries

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -2,38 +2,52 @@
   <div>
     <h1>Home</h1>
     <v-btn color="primary">Button</v-btn>
-    <ul v-for="post in getPosts" :key="post._id">
+    <div v-if="$apollo.loading">Loading...</div>
+    <ul v-else v-for="post in getPosts" :key="post._id">
       <li>
         {{post.title}}
         {{post.imageUrl}}
         {{post.descripton}}
       </li>
-      <li>
-        {{post.likes}}
-      </li>
+      <li>{{post.likes}}</li>
     </ul>
   </div>
 </template>
 
 <script>
-import {gql} from 'apollo-boost';
+import { gql } from "apollo-boost";
 
-export default{
-   name: "home",
-   apollo: {
-     getPosts:{
-       query: gql`
-          query{
-            getPosts{
-          _id,
-          title,
-          imageUrl,
-          description
+export default {
+  name: "home",
+  data(){
+    return{
+      posts: []
+    }
+  },
+  apollo: {
+    getPosts: {
+      query: gql`
+        query {
+          getPosts {
+            _id
+            title
+            imageUrl
+            description
           }
-         }
-       `
-     }
-   }
- }
-
+        }
+      `,
+      result(args){ //{data, loading, networkStatus}
+        // if(!loading){
+        //   this.posts = data.getPosts
+        //   console.log('[networkStatus]', networkStatus)
+        // }
+        console.dir(args);
+      },
+      error(err){
+        console.error('[ERROR!!]', err);
+        console.dir(err);
+      }
+    }
+  }
+};
 </script>
