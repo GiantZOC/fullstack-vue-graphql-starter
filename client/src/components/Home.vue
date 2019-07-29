0b67333d39414be14e2f467aee39cb0534126ec5
steven <steven@giantzoc.com>
2019-07-29 11:30:11 -0500

    Lesson 25 setting up Apollo Client

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -2,5 +2,38 @@
   <div>
     <h1>Home</h1>
     <v-btn color="primary">Button</v-btn>
+    <ul v-for="post in getPosts" :key="post._id">
+      <li>
+        {{post.title}}
+        {{post.imageUrl}}
+        {{post.descripton}}
+      </li>
+      <li>
+        {{post.likes}}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import {gql} from 'apollo-boost';
+
+export default{
+   name: "home",
+   apollo: {
+     getPosts:{
+       query: gql`
+          query{
+            getPosts{
+          _id,
+          title,
+          imageUrl,
+          description
+          }
+         }
+       `
+     }
+   }
+ }
+
+</script>
