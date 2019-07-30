720782c53943b88b0711497704984ba1b724fe3d
steven <steven@giantzoc.com>
2019-07-30 14:44:21 -0500

    Lesson 31 Loading with getters

--- client/src/store.js
+++ client/src/store.js
@@ -6,16 +6,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    posts: []
+    posts: [],
+    loading: false
   },
   mutations: {
     setPosts: (state, payload) =>{
       state.posts = payload;
+    },
+    setLoading: (state, payload) => {
+      state.loading = payload;
     }
   },
   actions: {
     getPosts: ({commit}) =>{
       // use apolloclient to fire getPosts query
+      commit('setLoading', true);
       apolloClient
         .query({
           query: gql`
@@ -31,14 +36,17 @@ export default new Vuex.Store({
           // Get data from actions to state via mutations
           // commit passes data from actions to mutation function
           commit('setPosts', data.getPosts);
+          commit('setLoading', false);
           console.log(data.getPosts);
 
         }).catch(err => {
+          commit('setLoading', false);
           console.error(err);
         })
     }
   },
   getters:{
-    posts: stats => state.posts
+    posts: state => state.posts,
+    loading: state => state.loading
   }
 })
